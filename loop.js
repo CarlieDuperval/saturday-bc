6d9d2e32ea59940ecfc0f35f26aa2180bcc3e3cc
Carlie Duperval <ritchellcarlie@gmail.com>
2022-07-23 11:37:03 -0400

    Added for loops file

---
+++ loop.js
@@ -0,0 +1,22 @@
+let students = [
+    {
+        name: 'max',
+        here: true
+    },
+    {
+        name:'pam',
+        here:'true'
+    }
+]
+
+function getStudentsNames(arr) {
+    let names = []
+
+    for (let i = 0; i < arr.length; i++){
+        names.push(arr[i].name)
+    
+    }
+    return names
+
+}
+console.log(getStudentsNames(students))
